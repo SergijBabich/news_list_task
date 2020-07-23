d87b704c630e150ee5c1a692d2548aac91a3673a
SerijBabich <moran.a@list.ru>
2020-07-23 23:25:07 +0300

    fix: changed on https

--- src/app/services/news_service.service.ts
+++ src/app/services/news_service.service.ts
@@ -6,7 +6,7 @@ import { map, catchError } from 'rxjs/operators';
 export class NewsService {
    
     constructor( private _http: HttpClient) {  }
-      baseUrl: string = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-06-23&sortBy=publishedAt&apiKey=7f811c3c0a824b3ab9bdad83e23c8031'
+      baseUrl: string = 'https://newsapi.org/v2/everything?q=bitcoin&from=2020-06-23&sortBy=publishedAt&apiKey=7f811c3c0a824b3ab9bdad83e23c8031'
     
       public getAllNews():Observable<object> { 
         return this._http.get(this.baseUrl)

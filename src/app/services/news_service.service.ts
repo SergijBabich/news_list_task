7b7dea60004ab2d35e83684cf798bb8db9e63d30
SerijBabich <moran.a@list.ru>
2020-07-24 08:56:05 +0300

    fix: changed initial data

--- src/app/services/news_service.service.ts
+++ src/app/services/news_service.service.ts
@@ -4,15 +4,19 @@ import { Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 @Injectable()
 export class NewsService {
-   
     constructor( private _http: HttpClient) {  }
-      baseUrl: string = 'https://newsapi.org/v2/everything?q=bitcoin&from=2020-06-23&sortBy=publishedAt&apiKey=7f811c3c0a824b3ab9bdad83e23c8031'
+    public now = new Date();
+    public day = this.now.getDate();
+    public month = this.now.getMonth();
+    public year = this.now.getFullYear();  
+           baseUrl: string = `http://newsapi.org/v2/everything?q=bitcoin&from=${this.year}-${this.month}-${this.day}&sortBy=publishedAt&apiKey=04900f9029a14dc2abf20de72096ca00`
     
-      public getAllNews():Observable<object> { 
+      public getAllNews():Observable<object> {
         return this._http.get(this.baseUrl)
         .pipe(catchError(err => {
           throw 'error in source. Details: ' + err;
         }))
+        
         
   }
 }
